efbf3631f5f48c46e1077fba31b851df8515aa53
hamze258 <hamze12@live.de>
2025-01-05 19:06:16 +0100

    dqn_3 fertig

--- vector_env/agents/reward3/DQN_vector_play_flappy.py
+++ vector_env/agents/reward3/DQN_vector_play_flappy.py
@@ -1,14 +1,14 @@
 #DQN_vector_plays_flappy.py
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3 import DQN
-from vector_env.agents.flappy_vector_env import FlappyBirdEnv
+from vector_env.agents.reward3.flappy_vector_env import FlappyBirdEnv
 import keyboard
 
 # Wrappen im VecEnv
 env = DummyVecEnv([lambda: FlappyBirdEnv(render_mode="rgb_array")])
 
 # Gelerntes Modell laden
-model = DQN.load(r"vector_env\models\DQN\training4\best_model.zip")
+model = DQN.load(r"vector_env\models\DQN\training8\best_model.zip")
 
 try:
     for i in range(100):  # Äußere Schleife für kontinuierliches Spielen
@@ -18,7 +18,8 @@ try:
             action, _ = model.predict(obs, deterministic=True)
             obs, reward, done, info = env.step(action)
             #time.sleep(0.02)
-            env.render()
+            #env.render()
+            print(info)
 
             if keyboard.is_pressed('q'):  # Prüfen, ob 'q' gedrückt wurde
                 print("Spiel manuell beendet.")
--- vector_env/training/reward3/train_vector_DQN.py
+++ vector_env/training/reward3/train_vector_DQN.py
@@ -230,7 +230,7 @@ if __name__ == "__main__":
     )
 
     # GPU oder CPU?
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cpu"
 
     # DQN-Modell
     model = DQN(
